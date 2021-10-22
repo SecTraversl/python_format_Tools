9cb91c5e3d4c276fcc51bf9eb782665e178a92e4
Travis Logue <SecTraversl@ProtonMail.com>
2021-10-22 07:31:06 -0700

    Last write time 2021-10-22

---
+++ format_header_break.py
@@ -0,0 +1,11 @@
+# %%
+#######################################
+def format_header_break():
+    """Returns a header break of 39 '#' glyphs.
+
+    Examples:
+        >>> format_header_break()\n
+        '#######################################'
+    """
+    return "{0:#<39}".format("")
+
